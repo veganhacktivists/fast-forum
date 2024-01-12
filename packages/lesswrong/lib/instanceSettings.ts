7301dfc3296a791bc29c049ff89026b63230fe0c
Joaquín Triñanes <joaquin@veganhacktivists.org>
2024-01-12 17:24:52 +0100

    Read APP_URL instead of settings

--- packages/lesswrong/lib/instanceSettings.ts
+++ packages/lesswrong/lib/instanceSettings.ts
@@ -1,29 +1,28 @@
-import { initializeSetting } from './publicSettings'
-import { isServer, isDevelopment, isAnyTest, getInstanceSettings, getAbsoluteUrl } from './executionEnvironment';
-import { pluralize } from './vulcan-lib/pluralize';
-import startCase from 'lodash/startCase' // AKA: capitalize, titleCase
-import { TupleSet, UnionOf } from './utils/typeGuardUtils';
+import { initializeSetting } from "./publicSettings";
+import { isServer, isDevelopment, isAnyTest, getInstanceSettings, getAbsoluteUrl } from "./executionEnvironment";
+import { pluralize } from "./vulcan-lib/pluralize";
+import startCase from "lodash/startCase"; // AKA: capitalize, titleCase
+import { TupleSet, UnionOf } from "./utils/typeGuardUtils";
 
 const getNestedProperty = function (obj: AnyBecauseTodo, desc: AnyBecauseTodo) {
-  var arr = desc.split('.');
-  while(arr.length && (obj = obj[arr.shift()]));
+  var arr = desc.split(".");
+  while (arr.length && (obj = obj[arr.shift()]));
   return obj;
 };
 
 // Is any one of the arguments an object
 const anyIsObject = (...args: any[]): boolean => {
-  return args.some(a => typeof a === 'object' && !Array.isArray(a) && a !== null)
-}
+  return args.some((a) => typeof a === "object" && !Array.isArray(a) && a !== null);
+};
 
-export const Settings: Record<string,any> = {};
+export const Settings: Record<string, any> = {};
 
 const getSetting = <T>(settingName: string, settingDefault?: T): T => {
-
   let setting;
   const instanceSettings = getInstanceSettings();
 
   // if a default value has been registered using registerSetting, use it
-  if (typeof settingDefault === 'undefined' && Settings[settingName])
+  if (typeof settingDefault === "undefined" && Settings[settingName])
     settingDefault = Settings[settingName].defaultValue;
 
   if (isServer) {
@@ -31,7 +30,7 @@ const getSetting = <T>(settingName: string, settingDefault?: T): T => {
     const rootSetting = getNestedProperty(instanceSettings, settingName);
     const privateSetting = instanceSettings.private && getNestedProperty(instanceSettings.private, settingName);
     const publicSetting = instanceSettings.public && getNestedProperty(instanceSettings.public, settingName);
-    
+
     // if setting is an object, "collect" properties from all three places
     if (anyIsObject(rootSetting, privateSetting, publicSetting)) {
       setting = {
@@ -41,27 +40,25 @@ const getSetting = <T>(settingName: string, settingDefault?: T): T => {
         ...publicSetting,
       };
     } else {
-      if (typeof rootSetting !== 'undefined') {
+      if (typeof rootSetting !== "undefined") {
         setting = rootSetting;
-      } else if (typeof privateSetting !== 'undefined') {
+      } else if (typeof privateSetting !== "undefined") {
         setting = privateSetting;
-      } else if (typeof publicSetting !== 'undefined') {
+      } else if (typeof publicSetting !== "undefined") {
         setting = publicSetting;
       } else {
         setting = settingDefault;
       }
     }
-
   } else {
     // look only in public
     const publicSetting = instanceSettings.public && getNestedProperty(instanceSettings.public, settingName);
-    setting = typeof publicSetting !== 'undefined' ? publicSetting : settingDefault;
+    setting = typeof publicSetting !== "undefined" ? publicSetting : settingDefault;
   }
 
   // Settings[settingName] = {...Settings[settingName], settingValue: setting};
 
   return setting;
-
 };
 
 /* 
@@ -82,28 +79,32 @@ const getSetting = <T>(settingName: string, settingDefault?: T): T => {
 */
 export class PublicInstanceSetting<SettingValueType> {
   constructor(
-    private settingName: string, 
+    private settingName: string,
     private defaultValue: SettingValueType,
-    private settingType: "optional" | "warning" | "required"
+    private settingType: "optional" | "warning" | "required",
   ) {
-    initializeSetting(settingName, "instance")
+    initializeSetting(settingName, "instance");
     if (isDevelopment && settingType !== "optional") {
-      const settingValue = getSetting(settingName)
-      if (typeof settingValue === 'undefined') {
+      const settingValue = getSetting(settingName);
+      if (typeof settingValue === "undefined") {
         if (settingType === "warning") {
           if (!isAnyTest) {
             // eslint-disable-next-line no-console
-            console.log(`No setting value provided for public instance setting for setting with name ${settingName} despite it being marked as warning`)
+            console.log(
+              `No setting value provided for public instance setting for setting with name ${settingName} despite it being marked as warning`,
+            );
           }
         }
         if (settingType === "required") {
-          throw Error(`No setting value provided for public instance setting for setting with name ${settingName} despite it being marked as required`)
-        } 
+          throw Error(
+            `No setting value provided for public instance setting for setting with name ${settingName} despite it being marked as required`,
+          );
+        }
       }
     }
   }
   get(): SettingValueType {
-    return getSetting(this.settingName, this.defaultValue)
+    return getSetting(this.settingName, this.defaultValue);
   }
 }
 
@@ -111,19 +112,23 @@ export class PublicInstanceSetting<SettingValueType> {
   Public Instance Settings
 */
 
-export const allForumTypes = new TupleSet(["LessWrong","AlignmentForum","EAForum"] as const);
+export const allForumTypes = new TupleSet(["LessWrong", "AlignmentForum", "EAForum"] as const);
 export type ForumTypeString = UnionOf<typeof allForumTypes>;
-export const forumTypeSetting = new PublicInstanceSetting<ForumTypeString>('forumType', 'LessWrong', 'warning') // What type of Forum is being run, {LessWrong, AlignmentForum, EAForum}
+export const forumTypeSetting = new PublicInstanceSetting<ForumTypeString>("forumType", "LessWrong", "warning"); // What type of Forum is being run, {LessWrong, AlignmentForum, EAForum}
 
-export const isLW = forumTypeSetting.get() === "LessWrong"
-export const isEAForum = forumTypeSetting.get() === "EAForum"
-export const isAF = forumTypeSetting.get() === "AlignmentForum"
-export const isLWorAF = isLW || isAF
+export const isLW = forumTypeSetting.get() === "LessWrong";
+export const isEAForum = forumTypeSetting.get() === "EAForum";
+export const isAF = forumTypeSetting.get() === "AlignmentForum";
+export const isLWorAF = isLW || isAF;
 
-export const forumTitleSetting = new PublicInstanceSetting<string>('title', 'LessWrong', 'warning') // Default title for URLs
+export const forumTitleSetting = new PublicInstanceSetting<string>("title", "LessWrong", "warning"); // Default title for URLs
 
 // Your site name may be referred to as "The Alignment Forum" or simply "LessWrong". Use this setting to prevent something like "view on Alignment Forum". Leave the article uncapitalized ("the Alignment Forum") and capitalize if necessary.
-export const siteNameWithArticleSetting = new PublicInstanceSetting<string>('siteNameWithArticle', "LessWrong", "warning")
+export const siteNameWithArticleSetting = new PublicInstanceSetting<string>(
+  "siteNameWithArticle",
+  "LessWrong",
+  "warning",
+);
 
 /**
  * Name of the tagging feature on your site. The EA Forum is going to try
@@ -131,65 +136,117 @@ export const siteNameWithArticleSetting = new PublicInstanceSetting<string>('sit
  * form of the name. We assume this is a single word currently. Spaces will
  * cause issues.
  */
-export const taggingNameSetting = new PublicInstanceSetting<string>('taggingName', 'tag', 'optional')
-export const taggingNameCapitalSetting = {get: () => startCase(taggingNameSetting.get())}
-export const taggingNamePluralSetting = {get: () => pluralize(taggingNameSetting.get())}
-export const taggingNamePluralCapitalSetting = {get: () => pluralize(startCase(taggingNameSetting.get()))}
-export const taggingNameIsSet = {get: () => taggingNameSetting.get() !== 'tag'}
+export const taggingNameSetting = new PublicInstanceSetting<string>("taggingName", "tag", "optional");
+export const taggingNameCapitalSetting = { get: () => startCase(taggingNameSetting.get()) };
+export const taggingNamePluralSetting = { get: () => pluralize(taggingNameSetting.get()) };
+export const taggingNamePluralCapitalSetting = { get: () => pluralize(startCase(taggingNameSetting.get())) };
+export const taggingNameIsSet = { get: () => taggingNameSetting.get() !== "tag" };
 
 // NB: Now that neither LW nor the EAForum use this setting, it's a matter of
 // time before it falls out of date. Nevertheless, I expect any newly-created
 // forums to use this setting.
-export const hasEventsSetting = new PublicInstanceSetting<boolean>('hasEvents', true, 'optional') // Whether the current connected server has events activated
+export const hasEventsSetting = new PublicInstanceSetting<boolean>("hasEvents", true, "optional"); // Whether the current connected server has events activated
 
-export const hasRejectedContentSectionSetting = new PublicInstanceSetting<boolean>('hasRejectedContentSection', false, 'optional');
+export const hasRejectedContentSectionSetting = new PublicInstanceSetting<boolean>(
+  "hasRejectedContentSection",
+  false,
+  "optional",
+);
 
 // Sentry settings
-export const sentryUrlSetting = new PublicInstanceSetting<string|null>('sentry.url', null, "warning"); // DSN URL
-export const sentryEnvironmentSetting = new PublicInstanceSetting<string|null>('sentry.environment', null, "warning"); // Environment, i.e. "development"
-export const sentryReleaseSetting = new PublicInstanceSetting<string|null>('sentry.release', null, "warning") // Current release, i.e. hash of lattest commit
-export const siteUrlSetting = new PublicInstanceSetting<string>('siteUrl', getAbsoluteUrl(), "optional")
+export const sentryUrlSetting = new PublicInstanceSetting<string | null>("sentry.url", null, "warning"); // DSN URL
+export const sentryEnvironmentSetting = new PublicInstanceSetting<string | null>("sentry.environment", null, "warning"); // Environment, i.e. "development"
+export const sentryReleaseSetting = new PublicInstanceSetting<string | null>("sentry.release", null, "warning"); // Current release, i.e. hash of lattest commit
+export const siteUrlSetting = new PublicInstanceSetting<string>(
+  "siteUrl",
+  process.env.APP_URL || "localhost",
+  "optional",
+);
 
 // FM Crossposting
-export const fmCrosspostSiteNameSetting = new PublicInstanceSetting<string|null>("fmCrosspost.siteName", null, "optional");
-export const fmCrosspostBaseUrlSetting = new PublicInstanceSetting<string|null>("fmCrosspost.baseUrl", null, "optional");
+export const fmCrosspostSiteNameSetting = new PublicInstanceSetting<string | null>(
+  "fmCrosspost.siteName",
+  null,
+  "optional",
+);
+export const fmCrosspostBaseUrlSetting = new PublicInstanceSetting<string | null>(
+  "fmCrosspost.baseUrl",
+  null,
+  "optional",
+);
 
 // For development, there's a matched set of CkEditor settings as instance
 // settings, which take precedence over the database settings. This allows
 // using custom CkEditor settings that don't match what's in the attached
 // database.
-export const ckEditorUploadUrlOverrideSetting = new PublicInstanceSetting<string | null>('ckEditorOverride.uploadUrl', null, "optional") // Image Upload URL for CKEditor
-export const ckEditorWebsocketUrlOverrideSetting = new PublicInstanceSetting<string | null>('ckEditorOverride.webSocketUrl', null, "optional") // Websocket URL for CKEditor (for collaboration)
+export const ckEditorUploadUrlOverrideSetting = new PublicInstanceSetting<string | null>(
+  "ckEditorOverride.uploadUrl",
+  null,
+  "optional",
+); // Image Upload URL for CKEditor
+export const ckEditorWebsocketUrlOverrideSetting = new PublicInstanceSetting<string | null>(
+  "ckEditorOverride.webSocketUrl",
+  null,
+  "optional",
+); // Websocket URL for CKEditor (for collaboration)
 
 // Stripe setting
 
 //Test vs Production Setting
-export const testServerSetting = new PublicInstanceSetting<boolean>("testServer", false, "warning")
+export const testServerSetting = new PublicInstanceSetting<boolean>("testServer", false, "warning");
 
 export const disableEnsureIndexSetting = new PublicInstanceSetting<boolean>("disableEnsureIndex", false, "optional");
 
 /** Currently LW-only; forum-gated in `userCanVote` */
-export const lowKarmaUserVotingCutoffDateSetting = new PublicInstanceSetting<string>("lowKarmaUserVotingCutoffDate", "11-30-2022", "optional");
+export const lowKarmaUserVotingCutoffDateSetting = new PublicInstanceSetting<string>(
+  "lowKarmaUserVotingCutoffDate",
+  "11-30-2022",
+  "optional",
+);
 /** Currently LW-only; forum-gated in `userCanVote` */
-export const lowKarmaUserVotingCutoffKarmaSetting = new PublicInstanceSetting<number>("lowKarmaUserVotingCutoffKarma", 1, "optional");
+export const lowKarmaUserVotingCutoffKarmaSetting = new PublicInstanceSetting<number>(
+  "lowKarmaUserVotingCutoffKarma",
+  1,
+  "optional",
+);
 
 /** Whether posts and other content is visible to non-logged-in users (TODO: actually implement this) */
 export const publicAccess = new PublicInstanceSetting<boolean>("publicAccess", true, "optional");
 
 /** Header-related settings */
-export const taglineSetting = new PublicInstanceSetting<string>('tagline', "A community blog devoted to refining the art of rationality", "warning")
-export const faviconUrlSetting = new PublicInstanceSetting<string>('faviconUrl', '/img/favicon.ico', "warning")
-export const faviconWithBadgeSetting = new PublicInstanceSetting<string|null>('faviconWithBadge', null, "optional")
-export const tabTitleSetting = new PublicInstanceSetting<string>('forumSettings.tabTitle', 'LessWrong', "warning")
-export const tabLongTitleSetting = new PublicInstanceSetting<string | null>('forumSettings.tabLongTitle', null, "optional")
+export const taglineSetting = new PublicInstanceSetting<string>(
+  "tagline",
+  "A community blog devoted to refining the art of rationality",
+  "warning",
+);
+export const faviconUrlSetting = new PublicInstanceSetting<string>("faviconUrl", "/img/favicon.ico", "warning");
+export const faviconWithBadgeSetting = new PublicInstanceSetting<string | null>("faviconWithBadge", null, "optional");
+export const tabTitleSetting = new PublicInstanceSetting<string>("forumSettings.tabTitle", "LessWrong", "warning");
+export const tabLongTitleSetting = new PublicInstanceSetting<string | null>(
+  "forumSettings.tabLongTitle",
+  null,
+  "optional",
+);
 
-export const noIndexSetting = new PublicInstanceSetting<boolean>('noindex', false, "optional")
+export const noIndexSetting = new PublicInstanceSetting<boolean>("noindex", false, "optional");
 
 /** Whether this forum verifies user emails */
 export const verifyEmailsSetting = new PublicInstanceSetting<boolean>("verifyEmails", true, "optional");
 
 export const hasCuratedPostsSetting = new PublicInstanceSetting<boolean>("hasCuratedPosts", false, "optional");
-export const hasSideCommentsSetting = new PublicInstanceSetting<boolean>("comments.sideCommentsEnabled", isLWorAF, "optional");
-export const hasCommentsTableOfContentSetting = new PublicInstanceSetting<boolean>("comments.tableOfContentsEnabled", isLWorAF, "optional");
+export const hasSideCommentsSetting = new PublicInstanceSetting<boolean>(
+  "comments.sideCommentsEnabled",
+  isLWorAF,
+  "optional",
+);
+export const hasCommentsTableOfContentSetting = new PublicInstanceSetting<boolean>(
+  "comments.tableOfContentsEnabled",
+  isLWorAF,
+  "optional",
+);
 export const hasDialoguesSetting = new PublicInstanceSetting<boolean>("dialogues.enabled", true, "optional");
-export const hasPostInlineReactionsSetting = new PublicInstanceSetting<boolean>("posts.inlineReactionsEnabled", isLWorAF, "optional");
+export const hasPostInlineReactionsSetting = new PublicInstanceSetting<boolean>(
+  "posts.inlineReactionsEnabled",
+  isLWorAF,
+  "optional",
+);
