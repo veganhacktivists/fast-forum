dd58b5f8663fb909cd0f4defb600508b37438876
Joaquín Triñanes <joaquin@veganhacktivists.org>
2024-01-12 18:22:24 +0100

    Remove analytics

--- packages/lesswrong/components/users/UsersEditForm.tsx
+++ packages/lesswrong/components/users/UsersEditForm.tsx
@@ -1,16 +1,21 @@
-import { Components, registerComponent, getFragment } from '../../lib/vulcan-lib';
-import { useMessages } from '../common/withMessages';
-import React from 'react';
-import { getUserEmail, userCanEditUser, userGetDisplayName, userGetProfileUrl} from '../../lib/collections/users/helpers';
-import Button from '@material-ui/core/Button';
-import { useCurrentUser } from '../common/withUser';
-import { gql, useMutation, useApolloClient } from '@apollo/client';
-import { isEAForum } from '../../lib/instanceSettings';
-import { useThemeOptions, useSetTheme } from '../themes/useTheme';
-import { captureEvent } from '../../lib/analyticsEvents';
-import { configureDatadogRum } from '../../client/datadogRum';
-import { preferredHeadingCase } from '../../themes/forumTheme';
-import { useNavigate } from '../../lib/reactRouterWrapper';
+import { Components, registerComponent, getFragment } from "../../lib/vulcan-lib";
+import { useMessages } from "../common/withMessages";
+import React from "react";
+import {
+  getUserEmail,
+  userCanEditUser,
+  userGetDisplayName,
+  userGetProfileUrl,
+} from "../../lib/collections/users/helpers";
+import Button from "@material-ui/core/Button";
+import { useCurrentUser } from "../common/withUser";
+import { gql, useMutation, useApolloClient } from "@apollo/client";
+import { isEAForum } from "../../lib/instanceSettings";
+import { useThemeOptions, useSetTheme } from "../themes/useTheme";
+import { captureEvent } from "../../lib/analyticsEvents";
+// import { configureDatadogRum } from '../../client/datadogRum';
+import { preferredHeadingCase } from "../../themes/forumTheme";
+import { useNavigate } from "../../lib/reactRouterWrapper";
 
 const styles = (theme: ThemeType): JssStyles => ({
   root: {
@@ -18,58 +23,54 @@ const styles = (theme: ThemeType): JssStyles => ({
     maxWidth: 600,
     margin: "auto",
     marginBottom: 100,
-    [theme.breakpoints.down('xs')]: {
+    [theme.breakpoints.down("xs")]: {
       width: "100%",
-    }
+    },
   },
 
   header: {
     margin: theme.spacing.unit * 2,
     marginBottom: theme.spacing.unit * 4,
-    [theme.breakpoints.down('md')]: {
-      marginLeft: theme.spacing.unit/2,
+    [theme.breakpoints.down("md")]: {
+      marginLeft: theme.spacing.unit / 2,
     },
   },
   resetButton: {
-    marginBottom:theme.spacing.unit * 4
-  }
-})
+    marginBottom: theme.spacing.unit * 4,
+  },
+});
 
 const passwordResetMutation = gql`
   mutation resetPassword($email: String) {
     resetPassword(email: $email)
   }
-`
+`;
 
-const UsersEditForm = ({terms, classes}: {
-  terms: {slug?: string, documentId?: string},
-  classes: ClassesType,
-}) => {
+const UsersEditForm = ({ terms, classes }: { terms: { slug?: string; documentId?: string }; classes: ClassesType }) => {
   const currentUser = useCurrentUser();
   const { flash } = useMessages();
   const navigate = useNavigate();
   const client = useApolloClient();
   const { Typography } = Components;
-  const [ mutate, loading ] = useMutation(passwordResetMutation, { errorPolicy: 'all' })
+  const [mutate, loading] = useMutation(passwordResetMutation, { errorPolicy: "all" });
   const currentThemeOptions = useThemeOptions();
   const setTheme = useSetTheme();
 
-  if(!terms.slug && !terms.documentId) {
+  if (!terms.slug && !terms.documentId) {
     // No user specified and not logged in
-    return (
-      <div className={classes.root}>
-        Log in to edit your profile.
-      </div>
-    );
+    return <div className={classes.root}>Log in to edit your profile.</div>;
   }
-  if (!userCanEditUser(currentUser,
-    terms.documentId ?
-      {_id: terms.documentId} :
-      // HasSlugType wants some fields we don't have (schemaVersion, _id), but
-      // userCanEdit won't use them
-      {slug: terms.slug, __collectionName: 'Users'} as HasSlugType
-  )) {
-    return <span>Sorry, you do not have permission to do this at this time.</span>
+  if (
+    !userCanEditUser(
+      currentUser,
+      terms.documentId
+        ? { _id: terms.documentId }
+        : // HasSlugType wants some fields we don't have (schemaVersion, _id), but
+          // userCanEdit won't use them
+          ({ slug: terms.slug, __collectionName: "Users" } as HasSlugType),
+    )
+  ) {
+    return <span>Sorry, you do not have permission to do this at this time.</span>;
   }
 
   // currentUser will not be the user being edited in the case where current
@@ -77,13 +78,14 @@ const UsersEditForm = ({terms, classes}: {
   // all in admin mode unfortunately. In the fullness of time we could fix that,
   // currently we disable it below
   const requestPasswordReset = async () => {
-    const { data } = await mutate({variables: { email: getUserEmail(currentUser) }})
-    flash(data?.resetPassword)
-  } 
+    const { data } = await mutate({ variables: { email: getUserEmail(currentUser) } });
+    flash(data?.resetPassword);
+  };
 
   // Since there are two urls from which this component can be rendered, with different terms, we have to
   // check both slug and documentId
-  const isCurrentUser = (terms.slug && terms.slug === currentUser?.slug) || (terms.documentId && terms.documentId === currentUser?._id)
+  const isCurrentUser =
+    (terms.slug && terms.slug === currentUser?.slug) || (terms.documentId && terms.documentId === currentUser?._id);
 
   return (
     <div className={classes.root}>
@@ -93,14 +95,11 @@ const UsersEditForm = ({terms, classes}: {
       {/* TODO(EA): Need to add a management API call to get the reset password
           link, but for now users can reset their password from the login
           screen */}
-      {isCurrentUser && !isEAForum && <Button
-        color="secondary"
-        variant="outlined"
-        className={classes.resetButton}
-        onClick={requestPasswordReset}
-      >
-        {preferredHeadingCase("Reset Password")}
-      </Button>}
+      {isCurrentUser && !isEAForum && (
+        <Button color="secondary" variant="outlined" className={classes.resetButton} onClick={requestPasswordReset}>
+          {preferredHeadingCase("Reset Password")}
+        </Button>
+      )}
 
       <Components.WrappedSmartForm
         collectionName="Users"
@@ -108,34 +107,33 @@ const UsersEditForm = ({terms, classes}: {
         removeFields={currentUser?.isAdmin ? [] : ["paymentEmail", "paymentInfo"]}
         successCallback={async (user: AnyBecauseTodo) => {
           if (user?.theme) {
-            const theme = {...currentThemeOptions, ...user.theme};
+            const theme = { ...currentThemeOptions, ...user.theme };
             setTheme(theme);
             captureEvent("setUserTheme", theme);
           }
 
           // reconfigure datadog RUM in case they have changed their settings
-          configureDatadogRum(user)
+          // configureDatadogRum(user)
 
           flash(`User "${userGetDisplayName(user)}" edited`);
           try {
-            await client.resetStore()
+            await client.resetStore();
           } finally {
-            navigate(userGetProfileUrl(user))
+            navigate(userGetProfileUrl(user));
           }
         }}
-        queryFragment={getFragment('UsersEdit')}
-        mutationFragment={getFragment('UsersEdit')}
+        queryFragment={getFragment("UsersEdit")}
+        mutationFragment={getFragment("UsersEdit")}
         showRemove={false}
       />
     </div>
   );
 };
 
-
-const UsersEditFormComponent = registerComponent('UsersEditForm', UsersEditForm, {styles});
+const UsersEditFormComponent = registerComponent("UsersEditForm", UsersEditForm, { styles });
 
 declare global {
   interface ComponentTypes {
-    UsersEditForm: typeof UsersEditFormComponent
+    UsersEditForm: typeof UsersEditFormComponent;
   }
 }
--- packages/lesswrong/lib/cookies/callbacks.ts
+++ packages/lesswrong/lib/cookies/callbacks.ts
@@ -1,5 +1,5 @@
 import { Cookies } from "react-cookie";
-import { initDatadog } from "../../client/datadogRum";
+// import { initDatadog } from "../../client/datadogRum";
 import { CallbackChainHook } from "../vulcan-lib";
 import { ALL_COOKIES, CookieType, isCookieAllowed } from "./utils";
 import { initReCaptcha } from "../../client/reCaptcha";
@@ -8,14 +8,16 @@ type CookiePreferencesChangedCallbackProps = {
   cookiePreferences: CookieType[];
   explicitlyChanged: boolean;
 };
-export const cookiePreferencesChangedCallbacks = new CallbackChainHook<CookiePreferencesChangedCallbackProps, []>("cookiePreferencesChanged");
+export const cookiePreferencesChangedCallbacks = new CallbackChainHook<CookiePreferencesChangedCallbackProps, []>(
+  "cookiePreferencesChanged",
+);
 /**
  * (Re)-initialise datadog RUM and ReCaptcha with the current cookie preferences.
  * NOTE: this will not turn it OFF if they have previously accepted and are now rejecting analytics cookies, it will only turn it ON if they are now accepting.
  * There is no way to turn it off without reloading currently (see https://github.com/DataDog/browser-sdk/issues/1008)
  */
 cookiePreferencesChangedCallbacks.add(() => {
-  void initDatadog();
+  // void initDatadog();
   void initReCaptcha();
 });
 
@@ -23,33 +25,35 @@ cookiePreferencesChangedCallbacks.add(() => {
  * Send a cookie_preferences_changed event to Google Tag Manager, which triggers google analytics and hotjar to start
  */
 cookiePreferencesChangedCallbacks.add(() => {
-  const dataLayer = (window as any).dataLayer
+  const dataLayer = (window as any).dataLayer;
   if (!dataLayer) {
     // eslint-disable-next-line no-console
-    console.warn("Trying to call gtag before dataLayer has been initialized")
+    console.warn("Trying to call gtag before dataLayer has been initialized");
   } else {
-    dataLayer.push({ event: "cookie_preferences_changed" })
+    dataLayer.push({ event: "cookie_preferences_changed" });
   }
 });
 
 /**
  * Remove all cookies that are not allowed
  */
-cookiePreferencesChangedCallbacks.add(({cookiePreferences, explicitlyChanged}: CookiePreferencesChangedCallbackProps) => {
-  // Don't try to remove any cookies if:
-  // - all cookies are allowed
-  // - this change was not explicitly made by the user (i.e. it was made based on their location)
-  if (!explicitlyChanged || JSON.stringify(cookiePreferences) === JSON.stringify(ALL_COOKIES)) return;
+cookiePreferencesChangedCallbacks.add(
+  ({ cookiePreferences, explicitlyChanged }: CookiePreferencesChangedCallbackProps) => {
+    // Don't try to remove any cookies if:
+    // - all cookies are allowed
+    // - this change was not explicitly made by the user (i.e. it was made based on their location)
+    if (!explicitlyChanged || JSON.stringify(cookiePreferences) === JSON.stringify(ALL_COOKIES)) return;
 
-  const cookies = new Cookies();
+    const cookies = new Cookies();
 
-  // eslint-disable-next-line no-console
-  console.log("Removing cookies that are not allowed");
+    // eslint-disable-next-line no-console
+    console.log("Removing cookies that are not allowed");
 
-  // remove all cookies that are not allowed
-  for (const cookieName in cookies.getAll()) {
-    if (!isCookieAllowed(cookieName, cookiePreferences)) {
-      cookies.remove(cookieName);
+    // remove all cookies that are not allowed
+    for (const cookieName in cookies.getAll()) {
+      if (!isCookieAllowed(cookieName, cookiePreferences)) {
+        cookies.remove(cookieName);
+      }
     }
-  }
-});
+  },
+);
