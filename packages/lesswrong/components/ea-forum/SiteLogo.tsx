5b8a3fbb6940c280d78ae967ba7f1652a65bd1b4
David van Beveren <7482732+davidvanbeveren@users.noreply.github.com>
2024-01-26 10:29:13 -0800

    Please come back online 😂

--- packages/lesswrong/components/ea-forum/SiteLogo.tsx
+++ packages/lesswrong/components/ea-forum/SiteLogo.tsx
@@ -47,7 +47,7 @@ const SiteLogoSmall = ({ eaWhite, classes }: { eaWhite?: boolean; classes: Class
       src={smallUrl}
       title={forumTitleSetting.get()}
       alt={`${forumTitleSetting.get()} Logo`}
-      style={{ width: '150px', height: '45px' }}
+      style={{ width: '150px', height: '46px' }}
     />
   );
 };
