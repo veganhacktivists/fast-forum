8801f451bed4bb4ad7be6b382f9747567f70972d
Joaquín Triñanes <joaquin@veganhacktivists.org>
2024-01-29 20:57:06 +0100

    Change site image

--- packages/lesswrong/components/vulcan-core/App.tsx
+++ packages/lesswrong/components/vulcan-core/App.tsx
@@ -1,28 +1,42 @@
-import React, { useEffect, useRef } from 'react';
-import moment from 'moment';
-import { DatabasePublicSetting, localeSetting } from '../../lib/publicSettings';
-import { Components, registerComponent, userChangedCallback } from '../../lib/vulcan-lib';
-import { TimeOverride, TimeContext } from '../../lib/utils/timeUtil';
+import React, { useEffect, useRef } from "react";
+import moment from "moment";
+import { DatabasePublicSetting, localeSetting } from "../../lib/publicSettings";
+import { Components, registerComponent, userChangedCallback } from "../../lib/vulcan-lib";
+import { TimeOverride, TimeContext } from "../../lib/utils/timeUtil";
 // eslint-disable-next-line no-restricted-imports
-import { useLocation, withRouter } from 'react-router';
-import { useQueryCurrentUser } from '../../lib/crud/withCurrentUser';
-import { LocationContext, parseRoute, ServerRequestStatusContext, SubscribeLocationContext, ServerRequestStatusContextType, NavigationContext } from '../../lib/vulcan-core/appContext';
-import type { RouterLocation } from '../../lib/vulcan-lib/routes';
-import { MessageContextProvider } from '../common/FlashMessages';
-import type { History } from 'history'
+import { useLocation, withRouter } from "react-router";
+import { useQueryCurrentUser } from "../../lib/crud/withCurrentUser";
+import {
+  LocationContext,
+  parseRoute,
+  ServerRequestStatusContext,
+  SubscribeLocationContext,
+  ServerRequestStatusContextType,
+  NavigationContext,
+} from "../../lib/vulcan-core/appContext";
+import type { RouterLocation } from "../../lib/vulcan-lib/routes";
+import { MessageContextProvider } from "../common/FlashMessages";
+import type { History } from "history";
 
-export const siteImageSetting = new DatabasePublicSetting<string>('siteImage', 'https://res.cloudinary.com/lesswrong-2-0/image/upload/v1654295382/new_mississippi_river_fjdmww.jpg') // An image used to represent the site on social media
+export const siteImageSetting = new DatabasePublicSetting<string>(
+  "siteImage",
+  "https://res.cloudinary.com/fast-forum/image/upload/v1706551810/vlqzlqumujk0zcceifrk.png",
+); // An image used to represent the site on social media
 
 interface ExternalProps {
-  apolloClient: AnyBecauseTodo,
-  serverRequestStatus?: ServerRequestStatusContextType,
-  timeOverride: TimeOverride,
+  apolloClient: AnyBecauseTodo;
+  serverRequestStatus?: ServerRequestStatusContextType;
+  timeOverride: TimeOverride;
 }
 
-const App = ({serverRequestStatus, timeOverride, history}: ExternalProps & {
-  history: History
+const App = ({
+  serverRequestStatus,
+  timeOverride,
+  history,
+}: ExternalProps & {
+  history: History;
 }) => {
-  const {currentUser, currentUserLoading} = useQueryCurrentUser();
+  const { currentUser, currentUserLoading } = useQueryCurrentUser();
   const reactDomLocation = useLocation();
   const locationContext = useRef<RouterLocation | null>(null);
   const subscribeLocationContext = useRef<RouterLocation | null>(null);
@@ -43,25 +57,23 @@ const App = ({serverRequestStatus, timeOverride, history}: ExternalProps & {
       iterator: currentUser,
       properties: [],
     });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentUser?._id]);
 
   // Parse the location into a route/params/query/etc.
-  const location = parseRoute({location: reactDomLocation});
+  const location = parseRoute({ location: reactDomLocation });
   if (location.redirected) {
-    return (
-      <Components.PermanentRedirect url={location.url} />
-    );
+    return <Components.PermanentRedirect url={location.url} />;
   }
 
   // Reuse the container objects for location and navigation context, so that
   // they will be reference-stable and won't trigger spurious rerenders.
   if (!locationContext.current) {
-    locationContext.current = {...location};
+    locationContext.current = { ...location };
   } else {
     Object.assign(locationContext.current, location);
   }
-  
+
   if (!navigationContext.current) {
     navigationContext.current = { history };
   } else {
@@ -71,12 +83,13 @@ const App = ({serverRequestStatus, timeOverride, history}: ExternalProps & {
   // subscribeLocationContext changes (by shallow comparison) whenever the
   // URL changes.
   // FIXME: Also needs to include changes to hash and to query params
-  if (!subscribeLocationContext.current ||
+  if (
+    !subscribeLocationContext.current ||
     subscribeLocationContext.current.pathname !== location.pathname ||
     JSON.stringify(subscribeLocationContext.current.query) !== JSON.stringify(location.query) ||
     subscribeLocationContext.current.hash !== location.hash
   ) {
-    subscribeLocationContext.current = {...location};
+    subscribeLocationContext.current = { ...location };
   } else {
     Object.assign(subscribeLocationContext.current, location);
   }
@@ -86,39 +99,37 @@ const App = ({serverRequestStatus, timeOverride, history}: ExternalProps & {
   // an Apollo cache containing the results of, the union of both logged-in
   // and logged-out views.)
   if (currentUserLoading && !currentUser) {
-    return (
-      <Components.Loading />
-    );
+    return <Components.Loading />;
   }
 
   return (
     <LocationContext.Provider value={locationContext.current}>
-    <NavigationContext.Provider value={navigationContext.current}>
-    <SubscribeLocationContext.Provider value={subscribeLocationContext.current}>
-    <ServerRequestStatusContext.Provider value={serverRequestStatus||null}>
-    <TimeContext.Provider value={timeOverride}>
-      <MessageContextProvider>
-        <Components.HeadTags image={siteImageSetting.get()} />
-        <Components.ScrollToTop />
-        <Components.Layout currentUser={currentUser}>
-          <location.RouteComponent />
-        </Components.Layout>
-      </MessageContextProvider>
-    </TimeContext.Provider>
-    </ServerRequestStatusContext.Provider>
-    </SubscribeLocationContext.Provider>
-    </NavigationContext.Provider>
+      <NavigationContext.Provider value={navigationContext.current}>
+        <SubscribeLocationContext.Provider value={subscribeLocationContext.current}>
+          <ServerRequestStatusContext.Provider value={serverRequestStatus || null}>
+            <TimeContext.Provider value={timeOverride}>
+              <MessageContextProvider>
+                <Components.HeadTags image={siteImageSetting.get()} />
+                <Components.ScrollToTop />
+                <Components.Layout currentUser={currentUser}>
+                  <location.RouteComponent />
+                </Components.Layout>
+              </MessageContextProvider>
+            </TimeContext.Provider>
+          </ServerRequestStatusContext.Provider>
+        </SubscribeLocationContext.Provider>
+      </NavigationContext.Provider>
     </LocationContext.Provider>
   );
-}
+};
 
-const AppComponent = registerComponent<ExternalProps>('App', App, {
+const AppComponent = registerComponent<ExternalProps>("App", App, {
   hocs: [withRouter],
 });
 
 declare global {
   interface ComponentTypes {
-    App: typeof AppComponent
+    App: typeof AppComponent;
   }
 }
 
