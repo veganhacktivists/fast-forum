8256321a598f246ffa3b726a8d771cd9409df1f6
Joaquín Triñanes <joaquin@veganhacktivists.org>
2024-03-11 14:27:33 +0100

    Prevent external forum URLs being replaced

--- packages/lesswrong/lib/routeUtil.tsx
+++ packages/lesswrong/lib/routeUtil.tsx
@@ -1,10 +1,16 @@
-import { isServer, getServerPort } from './executionEnvironment';
-import qs from 'qs';
-import React, { useCallback, useContext } from 'react';
-import { LocationContext, ServerRequestStatusContext, SubscribeLocationContext, ServerRequestStatusContextType, NavigationContext } from './vulcan-core/appContext';
-import type { RouterLocation } from './vulcan-lib/routes';
-import * as _ from 'underscore';
-import { ForumOptions, forumSelect } from './forumTypeUtils';
+import { isServer, getServerPort } from "./executionEnvironment";
+import qs from "qs";
+import React, { useCallback, useContext } from "react";
+import {
+  LocationContext,
+  ServerRequestStatusContext,
+  SubscribeLocationContext,
+  ServerRequestStatusContextType,
+  NavigationContext,
+} from "./vulcan-core/appContext";
+import type { RouterLocation } from "./vulcan-lib/routes";
+import { keys, indexOf } from "underscore";
+import { ForumOptions, forumSelect } from "./forumTypeUtils";
 
 // React Hook which returns the page location (parsed URL and route).
 // Return value contains:
@@ -33,22 +39,22 @@ import { ForumOptions, forumSelect } from './forumTypeUtils';
 // to rerender on navigations, use useSubscribedLocation instead.
 export const useLocation = (): RouterLocation => {
   return useContext(LocationContext)!;
-}
+};
 
 // React Hook which returns the server-side server request status, used to set 404s or redirects
 // The relevant handling happens in the renderPage function
 // This hook only works on the server and will throw an error when called on the client
-export const useServerRequestStatus = (): ServerRequestStatusContextType|null => {
-  return useContext(ServerRequestStatusContext)
-}
+export const useServerRequestStatus = (): ServerRequestStatusContextType | null => {
+  return useContext(ServerRequestStatusContext);
+};
 
 // React Hook which returns the page location, formatted as in useLocation, and
 // triggers a rerender whenever navigation occurs.
 export const useSubscribedLocation = (): RouterLocation => {
   return useContext(SubscribeLocationContext)!;
-}
+};
 
-export type NavigateFunction = AnyBecauseTodo
+export type NavigateFunction = AnyBecauseTodo;
 /**
  * React Hook which returns an acessor-object for page navigation. Contains one
  * field, `history`. See https://github.com/ReactTraining/history for
@@ -57,37 +63,35 @@ export type NavigateFunction = AnyBecauseTodo
  */
 export const useNavigate = (): NavigateFunction => {
   const { history } = useContext(NavigationContext);
-  return useCallback((url: string, options?: {replace?: boolean}) => {
-    if (options?.replace) {
-      history.replace(url);
-    } else {
-      history.push(url);
-    }
-  }, [history]);
-}
+  return useCallback(
+    (url: string, options?: { replace?: boolean }) => {
+      if (options?.replace) {
+        history.replace(url);
+      } else {
+        history.push(url);
+      }
+    },
+    [history],
+  );
+};
 
 // HoC which adds a `location` property to an object, which contains the page
 // location (parsed URL and route). See `useLocation`.
 export const withLocation = (WrappedComponent: any) => {
   return (props: AnyBecauseTodo) => (
     <LocationContext.Consumer>
-      {location =>
-        <WrappedComponent
-          {...props}
-          location={location}
-        />
-      }
+      {(location) => <WrappedComponent {...props} location={location} />}
     </LocationContext.Consumer>
   );
-}
+};
 
 export const getUrlClass = (): typeof URL => {
   if (isServer) {
-    return require('url').URL
+    return require("url").URL;
   } else {
-    return URL
+    return URL;
   }
-}
+};
 
 // Given a URL which might or might not have query parameters, return a URL in
 // which any query parameters found in queryParameterBlacklist are removed.
@@ -95,77 +99,79 @@ export const removeUrlParameters = (url: string, queryParameterBlacklist: string
   if (url.indexOf("?") < 0) return url;
   const [baseUrl, queryAndHash] = url.split("?");
   const [query, hash] = queryAndHash.split("#");
-  
+
   const parsedQuery = qs.parse(query);
   let filteredQuery: AnyBecauseTodo = {};
-  for (let key of _.keys(parsedQuery)) {
-    if (_.indexOf(queryParameterBlacklist, key) < 0) {
+  for (let key of keys(parsedQuery)) {
+    if (indexOf(queryParameterBlacklist, key) < 0) {
       filteredQuery[key] = parsedQuery[key];
     }
   }
-  
-  return baseUrl + (Object.keys(filteredQuery).length>0 ? '?'+qs.stringify(filteredQuery) : '') + (hash ? '#'+hash : '');
-}
+
+  return (
+    baseUrl +
+    (Object.keys(filteredQuery).length > 0 ? "?" + qs.stringify(filteredQuery) : "") +
+    (hash ? "#" + hash : "")
+  );
+};
 
 interface DomainList {
-  onsiteDomains: string[]
-  mirrorDomains: string[]
+  onsiteDomains: string[];
+  mirrorDomains: string[];
 }
 
 const LwAfDomainWhitelist: DomainList = {
   onsiteDomains: [
-    "lesswrong.com",
-    "lesserwrong.com",
-    "lessestwrong.com",
-    "alignmentforum.org",
-    "alignment-forum.com",
+    // "lesswrong.com",
+    // "lesserwrong.com",
+    // "lessestwrong.com",
+    // "alignmentforum.org",
+    // "alignment-forum.com",
     `localhost:${getServerPort()}`,
   ],
   mirrorDomains: [
-    "greaterwrong.com",
+    // "greaterwrong.com"
   ],
-}
+};
 
 const forumDomainWhitelist: ForumOptions<DomainList> = {
   LessWrong: LwAfDomainWhitelist,
   AlignmentForum: LwAfDomainWhitelist,
   EAForum: {
     onsiteDomains: [
-      'forum.effectivealtruism.org',
-      'forum-staging.effectivealtruism.org',
+      // 'forum.effectivealtruism.org',
+      // 'forum-staging.effectivealtruism.org',
       `localhost:${getServerPort()}`,
     ],
-    mirrorDomains: ['ea.greaterwrong.com'],
+    mirrorDomains: [
+      // 'ea.greaterwrong.com'
+    ],
   },
   default: {
-    onsiteDomains: [
-      `localhost:${getServerPort()}`,
-    ],
+    onsiteDomains: [`localhost:${getServerPort()}`],
     mirrorDomains: [],
-  }
-}
+  },
+};
 
-const domainWhitelist: DomainList = forumSelect(forumDomainWhitelist)
+const domainWhitelist: DomainList = forumSelect(forumDomainWhitelist);
 
-export const classifyHost = (host: string): "onsite"|"offsite"|"mirrorOfUs" => {
-  let urlType: "onsite"|"offsite"|"mirrorOfUs" = "offsite";
-  
+export const classifyHost = (host: string): "onsite" | "offsite" | "mirrorOfUs" => {
+  let urlType: "onsite" | "offsite" | "mirrorOfUs" = "offsite";
+
   // Returns true if two domains are either the same, or differ only by addition or removal of a "www."
   function isSameDomainModuloWWW(a: string, b: string) {
-    return a===b || "www."+a===b || a==="www."+b;
+    return a === b || "www." + a === b || a === "www." + b;
   }
 
   domainWhitelist.onsiteDomains.forEach((domain) => {
-    if (isSameDomainModuloWWW(host, domain))
-      urlType = "onsite";
-  })
+    if (isSameDomainModuloWWW(host, domain)) urlType = "onsite";
+  });
   domainWhitelist.mirrorDomains.forEach((domain) => {
-    if (isSameDomainModuloWWW(host, domain))
-      urlType = "mirrorOfUs";
-  })
+    if (isSameDomainModuloWWW(host, domain)) urlType = "mirrorOfUs";
+  });
 
   return urlType;
-}
+};
 
 // Returns whether a string could, conservatively, possibly be a database ID.
 // Used for disambiguating hashes, which in some URL formats could either point
@@ -173,4 +179,4 @@ export const classifyHost = (host: string): "onsite"|"offsite"|"mirrorOfUs" => {
 // A string could possibly be an ID if is in the range of meteor's `Random.id`.
 export const looksLikeDbIdString = (str: string): boolean => {
   return /^[a-zA-Z0-9]{17}$/.test(str);
-}
+};
