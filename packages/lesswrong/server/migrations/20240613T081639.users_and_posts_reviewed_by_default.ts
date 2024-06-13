6779eb9a92fb106d5fb46efe302a032240c67d88
Joaquín Triñanes <joaquin@veganhacktivists.org>
2024-06-13 17:53:32 +0200

    Update old values accordingly

--- packages/lesswrong/server/migrations/20240613T081639.users_and_posts_reviewed_by_default.ts
+++ packages/lesswrong/server/migrations/20240613T081639.users_and_posts_reviewed_by_default.ts
@@ -1,15 +1,61 @@
 import { Posts } from "../../lib/collections/posts";
+import { autoreviewerUserIdSetting } from "../../lib/instanceSettings";
 import Users from "../../lib/vulcan-users";
-import { updateDefaultValue, addField, dropField, dropDefaultValue } from "./meta/utils";
+import { updateDefaultValue, dropDefaultValue } from "./meta/utils";
 
 export const up = async ({ db }: MigrationContext) => {
-  await updateDefaultValue(db, Posts, "reviewedByUserId");
-  await updateDefaultValue(db, Users, "reviewedByUserId");
-  await updateDefaultValue(db, Users, "reviewedAt");
+  await Promise.all([
+    Posts.rawUpdateMany(
+      {
+        reviewedByUserId: null,
+      },
+      { $set: { reviewedByUserId: autoreviewerUserIdSetting.get() } },
+    ),
+    Users.rawUpdateMany(
+      {
+        reviewedByUserId: null,
+      },
+      { $set: { reviewedByUserId: autoreviewerUserIdSetting.get() } },
+    ),
+    Users.rawUpdateMany(
+      {
+        reviewedAt: null,
+      },
+      { $set: { reviewedAt: new Date() } },
+    ),
+  ]);
+
+  await Promise.all([
+    updateDefaultValue(db, Posts, "reviewedByUserId"),
+    updateDefaultValue(db, Users, "reviewedByUserId"),
+    updateDefaultValue(db, Users, "reviewedAt"),
+  ]);
 };
 
 export const down = async ({ db }: MigrationContext) => {
-  await dropDefaultValue(db, Posts, "reviewedByUserId");
-  await dropDefaultValue(db, Users, "reviewedByUserId");
-  await dropDefaultValue(db, Users, "reviewedAt");
+  await Promise.all([
+    dropDefaultValue(db, Posts, "reviewedByUserId"),
+    dropDefaultValue(db, Users, "reviewedByUserId"),
+    dropDefaultValue(db, Users, "reviewedAt"),
+  ]);
+
+  await Promise.all([
+    Posts.rawUpdateMany(
+      { reviewedByUserId: autoreviewerUserIdSetting.get() },
+      {
+        $set: {
+          reviewedByUserId: null,
+        },
+      },
+    ),
+    Users.rawUpdateMany(
+      { reviewedByUserId: autoreviewerUserIdSetting.get() },
+      {
+        $set: {
+          reviewedByUserId: null,
+          reviewedAt: null,
+        },
+      },
+    ),
+  ]);
 };
