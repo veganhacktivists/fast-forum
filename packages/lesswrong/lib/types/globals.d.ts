61f763a63237f538046d11354163ab5cf1cb799b
Joaquín Triñanes <joaquin@veganhacktivists.org>
2024-07-02 09:42:31 +0200

    Everything is almost functional
    I can't propagate bundleIsServer et. al to the other scripts tho :(

---
+++ packages/lesswrong/lib/types/globals.d.ts
@@ -0,0 +1,18 @@
+// declare var bundleIsServer: boolean;
+// declare var bundleIsTest: boolean;
+// declare var bundleIsProduction: boolean;
+// declare var bundleIsMigrations: boolean;
+// declare var defaultSiteAbsoluteUrl: string;
+// declare var serverPort: number;
+// declare var estrellaPid: number;
+declare global {
+  var bundleIsServer: boolean;
+  var bundleIsTest: boolean;
+  var bundleIsProduction: boolean;
+  var bundleIsMigrations: boolean;
+  var defaultSiteAbsoluteUrl: string;
+  var serverPort: number;
+  var estrellaPid: number;
+}
+
+export {};
