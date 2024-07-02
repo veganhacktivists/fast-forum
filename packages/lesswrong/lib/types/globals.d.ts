b254861c11dfd756354cb503e0205f612a6dac59
Joaquín Triñanes <joaquin@veganhacktivists.org>
2024-07-02 11:40:00 +0200

    Disable "auto refresh"

--- packages/lesswrong/lib/types/globals.d.ts
+++ packages/lesswrong/lib/types/globals.d.ts
@@ -1,10 +1,3 @@
-// declare var bundleIsServer: boolean;
-// declare var bundleIsTest: boolean;
-// declare var bundleIsProduction: boolean;
-// declare var bundleIsMigrations: boolean;
-// declare var defaultSiteAbsoluteUrl: string;
-// declare var serverPort: number;
-// declare var estrellaPid: number;
 declare global {
   var bundleIsServer: boolean;
   var bundleIsTest: boolean;
