8e35082a89db80950e9b093bb05ac2037f41639e
Joaquín Triñanes <joaquin@veganhacktivists.org>
2024-07-03 16:36:02 +0200

    Conditionally call google tag manager

--- packages/lesswrong/components/Layout.tsx
+++ packages/lesswrong/components/Layout.tsx
@@ -394,15 +394,17 @@ const Layout = ({
                             This website requires javascript to properly function. Consider activating javascript to get
                             access to all site functionality.{" "}
                           </noscript>
-                          {/* Google Tag Manager i-frame fallback */}
-                          <noscript>
-                            <iframe
-                              src={`https://www.googletagmanager.com/ns.html?id=${googleTagManagerIdSetting.get()}`}
-                              height="0"
-                              width="0"
-                              style={{ display: "none", visibility: "hidden" }}
-                            />
-                          </noscript>
+                          {googleTagManagerIdSetting.get() && (
+                            // Google Tag Manager i-frame fallback
+                            <noscript>
+                              <iframe
+                                src={`https://www.googletagmanager.com/ns.html?id=${googleTagManagerIdSetting.get()}`}
+                                height="0"
+                                width="0"
+                                style={{ display: "none", visibility: "hidden" }}
+                              />
+                            </noscript>
+                          )}
 
                           {!currentRoute?.standalone && (
                             <Header
