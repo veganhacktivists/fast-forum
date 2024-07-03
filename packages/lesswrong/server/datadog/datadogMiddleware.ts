69a9e37b4ca70943f52cff658209afc969481ed9
Joaquín Triñanes <joaquin@veganhacktivists.org>
2024-07-03 12:44:53 +0200

    Disable datadog

--- packages/lesswrong/server/datadog/datadogMiddleware.ts
+++ packages/lesswrong/server/datadog/datadogMiddleware.ts
@@ -1,6 +1,6 @@
-import { getDatadogUser } from "../../lib/collections/users/helpers";
+// import { getDatadogUser } from "../../lib/collections/users/helpers";
+// import tracer from "./tracer";
 import type { Request } from "express";
-import tracer from "./tracer";
 
 export const getIpFromRequest = (req: Request): string => {
   let ipOrIpArray =
@@ -16,25 +16,25 @@ export const getIpFromRequest = (req: Request): string => {
  * - Attach user info and IP address to the root span
  * - Allow the headers required for Real User Monitoring
  */
-export const datadogMiddleware = (req: AnyBecauseTodo, res: AnyBecauseTodo, next: AnyBecauseTodo) => {
-  const span = tracer.scope().active();
-  if (span !== null) {
-    // @ts-ignore - there is currently no public API for getting the root span, this is the accepted way (see https://github.com/DataDog/dd-trace-js/issues/725#issuecomment-805277510)
-    const rootSpan = span.context()._trace.started[0];
-    const user = req.user;
-    if (user) {
-      rootSpan.setTag("usr", getDatadogUser(user));
-    }
-
-    const ip = getIpFromRequest(req);
-
-    // Set the IP address as a tag on the root span
-    rootSpan.setTag("client.ip", ip);
-  }
-
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "x-datadog-trace-id, x-datadog-parent-id, x-datadog-origin, x-datadog-sampling-priority",
-  );
-  next();
-};
+// export const datadogMiddleware = (req: AnyBecauseTodo, res: AnyBecauseTodo, next: AnyBecauseTodo) => {
+//   const span = tracer.scope().active();
+//   if (span !== null) {
+//     // @ts-ignore - there is currently no public API for getting the root span, this is the accepted way (see https://github.com/DataDog/dd-trace-js/issues/725#issuecomment-805277510)
+//     const rootSpan = span.context()._trace.started[0];
+//     const user = req.user;
+//     if (user) {
+//       rootSpan.setTag("usr", getDatadogUser(user));
+//     }
+//
+//     const ip = getIpFromRequest(req);
+//
+//     // Set the IP address as a tag on the root span
+//     rootSpan.setTag("client.ip", ip);
+//   }
+//
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "x-datadog-trace-id, x-datadog-parent-id, x-datadog-origin, x-datadog-sampling-priority",
+//   );
+//   next();
+// };
