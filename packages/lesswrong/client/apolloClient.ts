c82669b959a0c95338ceb0181850dea868ebce4e
Joaquín Triñanes <joaquin@veganhacktivists.org>
2024-11-09 20:41:07 +0100

    Remove unused caches

--- packages/lesswrong/client/apolloClient.ts
+++ packages/lesswrong/client/apolloClient.ts
@@ -3,11 +3,12 @@ import { apolloCacheVoteablePossibleTypes } from "../lib/make_voteable";
 import { createHttpLink, createErrorLink, headerLink } from "../lib/apollo/links";
 
 export const createApolloClient = (baseUrl = "/"): ApolloClient<NormalizedCacheObject> => {
-  const cache = new InMemoryCache({
-    possibleTypes: {
-      ...apolloCacheVoteablePossibleTypes(),
-    },
-  });
+  const cache = new InMemoryCache();
+  //   {
+  //   possibleTypes: {
+  //     ...apolloCacheVoteablePossibleTypes(),
+  //   },
+  // }
 
   const cachedState = baseUrl === "/" ? window.__APOLLO_STATE__ : window.__APOLLO_FOREIGN_STATE__;
   cache.restore(cachedState ?? ""); // ssr
